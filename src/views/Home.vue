95126f677fb74d3cda8a2e2a87e75733aa621474
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-07-24 09:55:12 +0300

    feat: create (empty) base page CarDetails + configure router

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home container-fluid">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld,
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  min-height: 100vh;
+  background-color: #b8c6db;
+  background-image: linear-gradient(315deg, #b8c6db 0%, #f5f7fa 74%);
+}
+</style>
