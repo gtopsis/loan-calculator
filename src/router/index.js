bf0173ca191719f4f25def630316333e92e56e2a
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-07-24 19:17:53 +0300

    refactor: rename page Home to CarDetails

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import CarDetails from '../views/CarDetails.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'CarDetails',
+    component: CarDetails
   },
   // {
   //   path: '/about',
