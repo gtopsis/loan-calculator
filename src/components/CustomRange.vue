242b583205effec496298e60edd36b2a65c4c980
Giorgos Topsis <giorgostopsis@protonmail.com>
2021-07-25 09:19:46 +0300

    feat: create comp. customRange + style modal spacing and font

---
+++ src/components/CustomRange.vue
@@ -0,0 +1,68 @@
+<template>
+  <!-- Range -->
+  <div class="container-fluid px-0">
+    <div class="row justify-content-between">
+      <div class="col-auto ps-0">
+        <strong>
+          <label for="disabledRange" class="form-label mb-0">{{ label }}</label>
+        </strong>
+      </div>
+      <div class="col-auto pe-0 text-right">
+        <h4 class="mb-0">{{ currValue }}{{ measurementUnit }}</h4>
+      </div>
+    </div>
+    <div class="row">
+      <input
+        type="range"
+        class="form-range"
+        v-model="currValue"
+        :min="min"
+        :max="max"
+        :step="step"
+      />
+    </div>
+    <div class="row justify-content-between">
+      <div class="col-auto ps-0">
+        <span class="secondary text-left">{{ min }}{{ measurementUnit }}</span>
+      </div>
+      <div class="col-auto pe-0">
+        <span class="secondary text-right">{{ max }}{{ measurementUnit }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CustomRange",
+  props: {
+    min: { type: Number, default: 0 },
+    max: { type: Number, default: 0 },
+    step: { type: Number, default: 1 },
+    initValue: { type: Number, default: 0 },
+    label: { type: String, default: "" },
+    measurementUnit: { type: String, default: "" },
+  },
+  data() {
+    return {
+      currValue: 0,
+    };
+  },
+  computed: {},
+  methods: {},
+  mounted() {
+    this.currValue = this.initValue;
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.modal-header,
+.modal-footer {
+  border: none;
+}
+.form-label {
+  vertical-align: middle;
+}
+</style>
--- src/components/LoanCalcModal.vue
+++ src/components/LoanCalcModal.vue
@@ -3,15 +3,15 @@
   <div
     ref="modalRef"
     class="modal fade"
-    id="staticBackdrop"
     tabindex="-1"
     aria-labelledby="staticBackdropLabel"
     aria-hidden="true"
   >
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div
+      class="modal-dialog  modal-lg modal-dialog-centered modal-dialog-scrollable"
+    >
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
           <button
             type="button"
             class="btn-close"
@@ -19,17 +19,53 @@
             aria-label="Close"
           ></button>
         </div>
-        <div class="modal-body">
-          ...
+        <div class="modal-body px-sm-2 px-md-5">
+          <div class="container-fluid">
+            <div class="row mb-4">
+              <div class="col-12">
+                <h5 class="modal-title text-center">
+                  {{ modalTitle }}
+                </h5>
+              </div>
+              <div class="modal-subtitle col-12">
+                <p class="text-center">{{ modalSubtitle }}</p>
+              </div>
+            </div>
+
+            <!-- sliders -->
+            <div class="row mb-4">
+              <div class="col-12 mb-4">
+                <custom-range
+                  :min="sliderA.min"
+                  :max="sliderA.max"
+                  :label="sliderA.label"
+                  :step="sliderA.step"
+                  :initValue="sliderA.initValue"
+                  :measurementUnit="sliderA.measurementUnit"
+                ></custom-range>
+              </div>
+              <div class="col-12">
+                <custom-range
+                  :min="sliderB.min"
+                  :max="sliderB.max"
+                  :label="sliderB.label"
+                  :step="sliderB.step"
+                  :initValue="sliderB.initValue"
+                  :measurementUnit="sliderB.measurementUnit"
+                ></custom-range>
+              </div>
+            </div>
+
+            <div class="row justify-content-center">
+              <div class="col-auto">
+                <strong>
+                  Μηνιαία Δόση: <span>{{ total }}€</span>
+                </strong>
+              </div>
+            </div>
+          </div>
         </div>
         <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            Close
-          </button>
           <button type="button" class="btn btn-primary">Understood</button>
         </div>
       </div>
@@ -39,15 +75,46 @@
 
 <script>
 import { Modal } from "bootstrap";
+import CustomRange from "../components/CustomRange.vue";
+
 export default {
   name: "LoanCalcModal",
+  components: {
+    CustomRange,
+  },
   props: {
     isShow: { type: Boolean, default: false },
   },
   data() {
     return {
+      modalTitle: "Υπολογισμός Δόσης",
+      modalSubtitle: "Επίλεξε την προκαταβολή και την διαρκεια που σε συμφέρει",
       dialog: null,
+      finalPrice: 2000,
+      downsidePayment: 200,
+      duration: 12,
+      sliderA: {
+        min: 0,
+        step: 50,
+        max: 10700,
+        initValue: 4800,
+        label: "Προκαταβολή",
+        measurementUnit: "€",
+      },
+      sliderB: {
+        min: 0,
+        max: 72,
+        step: 1,
+        initValue: 72,
+        label: "Δόση",
+        measurementUnit: " μήνες",
+      },
     };
+  },
+  computed: {
+    total() {
+      return (this.finalPrice - this.downsidePayment) / this.duration;
+    },
   },
   methods: {
     showModal() {
@@ -55,8 +122,9 @@ export default {
     },
   },
   mounted() {
-    this.dialog = new Modal(this.$refs["modalRef"], { backdrop: true });
-    if (this.isShow == true) {
+    let refModal = this.$refs["modalRef"];
+    this.dialog = new Modal(refModal, { backdrop: true });
+    if (this.isShow === true) {
       this.showModal();
     }
   },
@@ -64,4 +132,18 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.modal-header,
+.modal-footer {
+  border: none;
+}
+
+.modal-title {
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+
+.modal-subtitle {
+  font-size: 0.8rem;
+}
+</style>
